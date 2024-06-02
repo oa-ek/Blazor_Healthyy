1266b91b025a5196148e2cd166baa1936c4f4b19
Ira-name <iryna.krush@oa.edu.ua>
2024-06-02 15:52:06 +0300

    add-list-category

---
+++ src/BlazorApp_Healthy/BlazorApp_Healthy/Server/Data/CategoryService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BlazorApp_Healthy.Server.Data;
+using BlazorApp_Healthy.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp_Healthy.Server.Services
+{
+    public class CategoryService
+    {
+        private readonly DataContext _context;
+
+        public CategoryService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Category>> GetAllCategoriesAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> AddCategoryAsync(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+    }
+}
