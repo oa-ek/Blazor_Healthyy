7b735a085e28665eeb31486c7476b99da708c719
Nastia Bondar <145715374+Nastiia18@users.noreply.github.com>
2024-06-02 23:22:13 +0300

    recipe-con-ser

--- src/BlazorApp_Healthy/BlazorApp_Healthy/Server/Data/RecipeService.cs
+++ src/BlazorApp_Healthy/BlazorApp_Healthy/Server/Data/RecipeService.cs
@@ -16,11 +16,56 @@ namespace BlazorApp_Healthy.Server.Data
             return await _context.Recipes.ToListAsync();
         }
 
+        public async Task<Recipe> GetRecipeByIdAsync(Guid id)
+        {
+            return await _context.Recipes.FindAsync(id);
+        }
+
         public async Task<Recipe> AddRecipeAsync(Recipe recipe)
         {
             _context.Recipes.Add(recipe);
             await _context.SaveChangesAsync();
             return recipe;
+        }
+
+        public async Task<bool> UpdateRecipeAsync(Recipe recipe)
+        {
+            _context.Entry(recipe).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RecipeExists(recipe.Id))
+                {
+                    return false;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        public async Task<bool> DeleteRecipeAsync(Recipe recipe)
+        {
+            var recipeToDelete = await _context.Recipes.FindAsync(recipe.Id);
+            if (recipeToDelete == null)
+            {
+                return false;
+            }
+
+            _context.Recipes.Remove(recipeToDelete);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        private bool RecipeExists(Guid id)
+        {
+            return _context.Recipes.Any(e => e.Id == id);
         }
     }
 }
