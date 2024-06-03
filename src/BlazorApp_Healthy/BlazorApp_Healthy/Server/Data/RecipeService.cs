ab7a07b807480ebdb80d324fdc69c700e86b8c14
Nastia Bondar <145715374+Nastiia18@users.noreply.github.com>
2024-06-03 14:42:14 +0300

    Deteils-recipe

--- src/BlazorApp_Healthy/BlazorApp_Healthy/Server/Data/RecipeService.cs
+++ src/BlazorApp_Healthy/BlazorApp_Healthy/Server/Data/RecipeService.cs
@@ -18,9 +18,11 @@ namespace BlazorApp_Healthy.Server.Data
 
         public async Task<Recipe> GetRecipeByIdAsync(Guid id)
         {
-            return await _context.Recipes.FindAsync(id);
+            return await _context.Recipes
+                .Include(r => r.Categories)
+                .Include(r => r.Ingredients)
+                .FirstOrDefaultAsync(r => r.Id == id);
         }
-
         public async Task<Recipe> AddRecipeAsync(Recipe recipe)
         {
             _context.Recipes.Add(recipe);
